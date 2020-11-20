d848d16ddfcc11b2e3cf95c483930fa1397eaa74
paveleveline <evelyn_lazau@yahoo.com>
2020-11-21 01:54:57 +0200

    finished online-shopping layout

--- resources/main.js
+++ resources/main.js
@@ -1,51 +1,58 @@
 $(function() { 
 
-let pictureSelected = [];
-
-const photosContentWrapper = $('.photos-content-wrapper');
-const overlayContentWrapper = $('.overlay-content-wrapper');
-const cartSelection = $('.cart-selection'); 
-// ti-ar fi adus puncte frumoase sa implementezi click-ul si pe butonul asta si ar fi fost ceva foarte simplu pentru tine
-const closeBtn = $('.close-icon');
-const showOverlay = $('.overlay');
-
-productCoatObj = products.coats;
-console.log(productCoatObj)
-
-getproductHTML = function(productCoatObj){
-    return `<div class="single-coat-wrapper" data-id=${productCoatObj.id}>
-    <div class="picture-coat" style="background-image: url(assets/coats/${productCoatObj.imgUrl})"></div>
-    <div class="name-and-price-wrapper">
-        <div class="name-coat">${productCoatObj.name}</div>
-        <div class="price-coat">${productCoatObj.currency} ${productCoatObj.price}</div>
+    let pictureSelected = $('.picture-coat');
+    
+    const photosContentWrapper = $('.photos-content-wrapper');
+    const overlayContentWrapper = $('.overlay-content-wrapper');
+    const cartSelection = $('.cart-selection'); 
+    const closeBtn = $('.close-icon');
+    const showOverlay = $('.overlay');
+    
+    productCoatObj = products.coats;
+    console.log(productCoatObj)
+    
+    getproductHTML = function(index, productCoatObj){
+        return `<div class="single-coat-wrapper" data-index=${index} data-id=${productCoatObj.id}>
+        <div class="picture-coat" style="background-image: url(assets/coats/${productCoatObj.imgUrl})"></div>
+        <div class="name-and-price-wrapper">
+            <div class="name-coat">${productCoatObj.name}</div>
+            <div class="price-coat">${productCoatObj.currency} ${productCoatObj.price}</div>
+        </div>
+    
     </div>
-
-</div>
-    `;
-};
-for(let i=0; i<products.coats.length; i++){
-    let productObj = products.coats[i];
-    productHTML = getproductHTML(productObj);
-    photosContentWrapper.append(productHTML);
-    }
-
-    photosContentWrapper.delegate('.picture-coat', "click", function(){
-        const id = $(this).parents('.single-coat-wrapper').data('id');
-        
-        // nu ai nevoie de sectiunea asta in proiectul asta. Dincolo aveai nevoie pentru ca aveai un array de favorites unde voiai sa stii ce ai adaugat, dar aici ai un simplu click un overlay care arata datele respective
-        if(jQuery.inArray(id, pictureSelected) === -1){
-            pictureSelected.push(id);
-        }else { 
-            pictureSelected.splice(pictureSelected.indexOf(id), 1);
+        `;
+    };
+    for(let i=0; i<products.coats.length; i++){
+        let productObj = products.coats[i];
+        productHTML = getproductHTML(i, productObj);
+        photosContentWrapper.append(productHTML);
         }
-            console.log(pictureSelected);
+    
+        photosContentWrapper.delegate('.picture-coat',"click", function(){
+        const id = $(this).parents('.single-coat-wrapper').data('id');
+        console.log('id is: ',  id);
+        const index = $(this).parents('.single-coat-wrapper').data('index');
+        console.log('index is: ', index);
+       
+        showOverlay.removeClass('hidden');
+       
+       const productIndex = products.coats[index];
+        console.log(productIndex);
+        const namePriceWrapper = $('.name-price-size-wrapper');
+        const compositionCountryWrapper = $('.composition-and-country-wrapper');
         
-            if(pictureSelected.length>0 ){
-                showOverlay.removeClass('hidden');
-            } else{
-                showOverlay.addClass('hidden');
-            }
+        cartSelection.css({backgroundImage: "url(assets/coats/" + productIndex.imgUrl + ")"});   
+        namePriceWrapper.find('.name').text(productIndex.name);
+        namePriceWrapper.find('.price').text(productIndex.currency + productIndex.price);
+        compositionCountryWrapper.find('.composition').children().text(productIndex.composition);
+        compositionCountryWrapper.find('.country').children().text(productIndex.country);
+        $('.care-wrapper div').text(productIndex.care);
+         
+        });
+    
+        closeBtn.click(function(){
+        showOverlay.addClass('hidden');
+        });
+    
     });
-
-
-});
+    
